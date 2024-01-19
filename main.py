6c25dd151e35ace159564ac17f8404839b97994b
Karolina Kozubik <k4aro0@gmail.com>
2024-01-19 21:36:40 +0100

    generate html report

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from app.Game import Game
 
 if __name__ == "__main__":
     try:
-        counter = int(sys.argv[1])
+        games_num = int(sys.argv[1])
     except IndexError:
         print("nie podano danych wejściowych")
     except ValueError:
         print("nieprawidłowe dane wejściowe")
     game = Game()
-    while counter != 0 and not game.get_result():
-        counter -= 1
+    counter = 0
+    while counter < games_num and not game.get_result():
+        counter += 1
         game = Game()
         game.run()
     print(game)
