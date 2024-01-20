26ad49b1245dbb2f140a21e147e8d27ad804d9b5
Karolina Kozubik <k4aro0@gmail.com>
2024-01-20 17:44:48 +0100

    generate backup

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ wd=$(dirname $0);
 files=()
 
 for file in ./in/*; do
-    file_number=${file:7:1}
-    file_name=./out/out$file_number.txt
+    file_number=${file#./in/in}
+    file_name=./out/out$file_number
     games_counter=$(cat $file)
     python -m main $games_counter > $file_name
     files+=($file_name)
 done
 
 python -m html_report ${files[*]} > ./report/index.html
+python -m backup
